2067596e7d4a2768650817334450b20fac7dfb1f
Kenichiro Mori <kemori@redhat.com>
2024-07-16 13:52:22 +0900

    update

--- http_deposit/src/main/java/com/redhat/example/entity/KijitsuNyukinRequestEntity.java
+++ http_deposit/src/main/java/com/redhat/example/entity/KijitsuNyukinRequestEntity.java
@@ -2,10 +2,12 @@ package com.redhat.example.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.stereotype.Component;
 import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
+@Component
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KijitsuNyukinRequestEntity {
 
